92b57fb2124f4e62557797e75c5440dce036e1b1
SyedAzherAli <142889706+SyedAzherAli@users.noreply.github.com>
2025-02-02 06:46:53 +0530

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -4,9 +4,6 @@ import resume from '../../Assets/../Assets/Syed Azher Ali - AWS Devops Engineer.
 const Portfolio = () => {
   return (
     <div>
-      <h1>Welcome to My Portfolio</h1>
-      
-      <p>Check out my resume:</p>
       
       {/* Embed PDF directly on the webpage */}
       <embed
